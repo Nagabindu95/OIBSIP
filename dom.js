d2859b15b611f3478a1f3ec303e3a95747921ee2
Naga bindu <nagabindubindu95@gmail.com>
2025-02-18 23:03:54 +0530

    Removed unnecessary files

--- dom.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('menu-icon').addEventListener('click', function() {
-    const navMenu = document.getElementById('nav-menu');
-    if (navMenu.style.display === 'block') {
-        navMenu.style.display = 'none';
-    } else {
-        navMenu.style.display = 'block';
-    }
-});
-
