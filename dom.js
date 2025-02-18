ef6c75ef3d6efdd4623b040a6d792895a376e11e
Naga bindu <nagabindubindu95@gmail.com>
2025-02-18 20:03:19 +0530

    Set up project with HTML, CSS, and JS files

---
+++ dom.js
@@ -0,0 +1,9 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    const navMenu = document.getElementById('nav-menu');
+    if (navMenu.style.display === 'block') {
+        navMenu.style.display = 'none';
+    } else {
+        navMenu.style.display = 'block';
+    }
+});
+
